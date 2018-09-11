d41963ef5b004f5d6e294be20a1576db7a2891d3
Oliver Tonnhofer <olt@bogosoft.com>
2018-09-11 11:02:41 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="svgserver",
+    description="Render layered SVG files with MapServer",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": []
+    },
+    extras_require={},
+    install_requires=["Werkzeug"],
+)
